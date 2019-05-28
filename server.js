caa9f9d1f2eb4b96f01f32b4372b71c59f1aa720
Fatih Nar <fenar@yahoo.com>
2019-05-28 08:45:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hello Folks For GCP Webinar 2019 V1.2!");
+  response.end("Hello Folks For GCP Webinar 2019 V1.0!");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
